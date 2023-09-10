321fb7706357276997347ab50b6239a77135d48a
uddasp <uddasp@gmail.com>
2023-09-10 11:47:35 +0530

    ""

--- .github/workflows/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "uddasp-tfstate"
-    key            = "pipeline.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform_locks"
-  }
-}
